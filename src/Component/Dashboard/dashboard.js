91304b796bcb92f29caf81bdd2e1ac3e7e3b2dae
theghostyousee <patrickserise174@gmail.com>
2023-04-19 13:52:27 +0200

    s

--- src/Component/Dashboard/dashboard.js
+++ src/Component/Dashboard/dashboard.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import './dashboard.css'
+import React, { useState, useEffect } from "react";
+import "./dashboard.css";
 import Coin from "../../Media/coin.svg";
 import Dollars from "../../Media/dollars.svg";
 import Pourcentage from "../../Media/percentage.svg";
@@ -11,10 +11,6 @@ import contractABI from "./contractABI.json";
 function Dashboard() {
   const [amount, setAmount] = useState("");
   const [connected, setConnected] = useState(false);
-  const [balance, setBalance] = useState(0);
-  const [stackValue, setStackValue] = useState("");
-
-  const ETH_TO_STACK_RATIO = 23668.122;
 
   const connectMetaMask = async () => {
     if (typeof window.ethereum !== "undefined") {
@@ -31,7 +27,6 @@ function Dashboard() {
       console.error("Please install MetaMask");
     }
   };
-
 
   const handleMaxClick = async () => {
     const web3 = new Web3(window.ethereum);
@@ -81,113 +76,118 @@ function Dashboard() {
   }, []);
 
   return (
-    <div className='dashboard-wrap'>
-      <div className='top-dashboard'>
-        <div className='welcome'>
+    <div className="dashboard-wrap">
+      <div className="top-dashboard">
+        <div className="welcome">
           <p>Welcome!</p>
           <h1>This is your Paddas dashboard</h1>
         </div>
-        <div className='rebase'>
+        <div className="rebase">
           <p>Next Rebase</p>
           <h1>00 : 00 : 00</h1>
         </div>
       </div>
-      <div className='card-dashboard'>
-
-        <div className='card'>
-          <div className='top-card'>
+      <div className="card-dashboard">
+        <div className="card">
+          <div className="top-card">
             <img src={Coin}></img>
           </div>
-          <div className='bottom-card'>
+          <div className="bottom-card">
             <p>Market Cap</p>
             <h2>$0</h2>
           </div>
         </div>
-        <div className='card'>
-          <div className='top-card'>
+        <div className="card">
+          <div className="top-card">
             <img src={Pourcentage}></img>
           </div>
-          <div className='bottom-card'>
+          <div className="bottom-card">
             <p>Current APY</p>
             <h2>160 000.02%</h2>
           </div>
         </div>
-        <div className='card'>
-          <div className='top-card'>
+        <div className="card">
+          <div className="top-card">
             <img src={Dollars}></img>
           </div>
-          <div className='bottom-card'>
+          <div className="bottom-card">
             <p>USD Market Price</p>
             <h2>$0</h2>
           </div>
         </div>
-
       </div>
 
-      <div className='presale-banner'>
-        <div className='presale-card'>
-          <div className='presale-top'>
+      <div className="presale-banner">
+        <div className="presale-card">
+          <div className="presale-top">
             <h2>Paddas Sale</h2>
           </div>
-          <div className='input-buy'>
-            <input type={'number'} placeholder='Amount of $ETH'                 value={amount}
-                onChange={handleAmountChange}></input>
+          <div className="input-buy">
+            <input
+              type={"number"}
+              placeholder="Amount of $ETH"
+              value={amount}
+              onChange={handleAmountChange}
+            ></input>
           </div>
-          <div className='info-presale'>
-            <div className='presale-row'>
+          <div className="info-presale">
+            <div className="presale-row">
               <p>PADDAS Received</p>
               <p>0</p>
             </div>
-            <div className='presale-row'>
+            <div className="presale-row">
               <p>Discount Price</p>
               <p>-10%</p>
             </div>
-            <div className='presale-row'>
+            <div className="presale-row">
               <p>Max Wallet</p>
               <p>1.5 ETH</p>
             </div>
           </div>
-          <div className='button-presale'>
-          {connected ? (
-                <button onClick={handleUnlockWallet}>Buy Discounted $PADDAS</button>
-              ) : (
-                <button onClick={connectMetaMask}>Approve</button>
-              )}
+          <div className="button-presale">
+            {connected ? (
+              <button onClick={handleUnlockWallet}>
+                Buy Discounted $PADDAS
+              </button>
+            ) : (
+              <button onClick={connectMetaMask}>Approve</button>
+            )}
           </div>
-
         </div>
       </div>
-      
-      <div className='wallet-info'>
-        <div className='top-wallet'>
+
+      <div className="wallet-info">
+        <div className="top-wallet">
           <img src={Wallet}></img>
         </div>
-        <div className='wallet'>
-          <div className='row'>
+        <div className="wallet">
+          <div className="row">
             <p>Your wallet balance</p>
             <p>0</p>
           </div>
-          <div className='row'>
+          <div className="row">
             <p>Next Reward Amount</p>
             <p>0</p>
           </div>
-          <div className='row'>
+          <div className="row">
             <p>Next Reward Yield</p>
             <p>0%</p>
           </div>
-          <div className='row'>
+          <div className="row">
             <p>ROI (5-Day Rate)</p>
             <p>0%</p>
           </div>
         </div>
-        <div className='message'>
+        <div className="message">
           <img src={Panel}></img>
-          <p>We recommend connecting your wallet in order to see more information.</p>
+          <p>
+            We recommend connecting your wallet in order to see more
+            information.
+          </p>
         </div>
-
       </div>
     </div>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
